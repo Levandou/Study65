40e952e41dc0cc39def3cef8e3b9d424b77dc4e3
Levandou <velagissellint@gmail.com>
2021-12-23 00:10:57 +0400

    Добавлена загрузка данных с помощью rxJava3, а также индикатор загрузки данныхх

--- app/src/main/java/com/velagissellint/a65/presentation/contactDetails/ContactDetailsFragment.kt
+++ app/src/main/java/com/velagissellint/a65/presentation/contactDetails/ContactDetailsFragment.kt
@@ -9,18 +9,19 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CompoundButton
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.Switch
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.os.bundleOf
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.velagissellint.a65.R
@@ -43,7 +44,6 @@ class ContactDetailsFragment : Fragment(), CompoundButton.OnCheckedChangeListene
         arguments?.let {
             id = it.getInt(ID_ARG)
         }
-        Log.d("qwery",id.toString())
     }
 
     override fun onCreateView(
@@ -57,9 +57,11 @@ class ContactDetailsFragment : Fragment(), CompoundButton.OnCheckedChangeListene
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-         contactDetailsViewModel.getContact(id)
+        contactDetailsViewModel.getContact(id)
+        val progressBar = view.findViewById<ProgressBar>(R.id.progressBar)
         switchAlarm = requireView().findViewById(R.id.SwitchBirthday)
         switchAlarm?.setOnCheckedChangeListener(this)
+        observeProgressBar(progressBar)
         val permission =
             ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_CONTACTS);
         if (permission == PackageManager.PERMISSION_GRANTED) {
@@ -67,6 +69,14 @@ class ContactDetailsFragment : Fragment(), CompoundButton.OnCheckedChangeListene
         } else {
             getPermission()
         }
+    }
+
+    private fun observeProgressBar(progressBar: ProgressBar) {
+        contactDetailsViewModel
+            .isLoadingPublic
+            .observe(viewLifecycleOwner, {
+                progressBar.isVisible= it
+            })
     }
 
     @SuppressLint("UseSwitchCompatOrMaterialCode")
@@ -78,7 +88,7 @@ class ContactDetailsFragment : Fragment(), CompoundButton.OnCheckedChangeListene
             val email = requireView().findViewById<TextView>(R.id.email)
             val description = requireView().findViewById<TextView>(R.id.description)
             val switchNotify = requireView().findViewById<Switch>(R.id.SwitchBirthday)
-            it?.let {contact->
+            it?.let { contact ->
                 activity?.runOnUiThread {
                     ivPhoto.setImageURI(Uri.parse(contact.imageResource))
                     tvName.text = contact.fullName
@@ -112,7 +122,7 @@ class ContactDetailsFragment : Fragment(), CompoundButton.OnCheckedChangeListene
         }
     }
 
-    fun isAlarmSet(context: Context): Boolean {
+    private fun isAlarmSet(context: Context): Boolean {
         val intent = Intent(context, BroadcastReceiverForNotify::class.java)
 
         val alarmIntent = id?.let {
@@ -167,7 +177,7 @@ class ContactDetailsFragment : Fragment(), CompoundButton.OnCheckedChangeListene
         super.onDestroyView()
     }
 
-    fun getPermission() {
+    private fun getPermission() {
         requestPermissions(
             arrayOf(Manifest.permission.READ_CONTACTS),
             PERMISSIONS_REQUEST_READ_CONTACTS
--- app/src/main/java/com/velagissellint/a65/presentation/contactDetails/ContactDetailsViewModel.kt
+++ app/src/main/java/com/velagissellint/a65/presentation/contactDetails/ContactDetailsViewModel.kt
@@ -1,5 +1,6 @@
 package com.velagissellint.a65.presentation.contactDetails
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -7,16 +8,39 @@ import com.velagissellint.a65.data.ContactsRepository
 import com.velagissellint.a65.domain.DetailedInformationAboutContact
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.kotlin.addTo
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 @HiltViewModel
 class ContactDetailsViewModel @Inject constructor(
     private val contactsRepository: ContactsRepository,
 ) : ViewModel() {
+    private val disposable: CompositeDisposable = CompositeDisposable()
     private val mutableContact = MutableLiveData<DetailedInformationAboutContact?>()
-    var contact = mutableContact as LiveData<DetailedInformationAboutContact?>
+    val contact = mutableContact as LiveData<DetailedInformationAboutContact?>
+    private val isLoading = MutableLiveData<Boolean>()
+    val isLoadingPublic=isLoading as LiveData<Boolean>
 
     fun getContact(id: Int?) {
-        val detailedInformationAboutContact = id?.let { contactsRepository.getContact(it) }
-        mutableContact.value = detailedInformationAboutContact
+        id?.let { idAfterLet ->
+            contactsRepository.getContact(idAfterLet)
+                .subscribeOn(Schedulers.io())
+                .doOnSubscribe { isLoading.postValue(true) }
+                .doFinally{isLoading.postValue(false)}
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    mutableContact.value = it
+                }, {
+                    Log.d("ERRORO", it.message.toString())
+                })
+                .addTo(disposable)
+        }
+    }
+
+    override fun onCleared() {
+        disposable.dispose()
+        super.onCleared()
     }
 }
